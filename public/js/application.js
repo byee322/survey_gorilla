cdfdc168da67ebda8cb2d4efdffc49d983d427ac
Brian Yee <byee322@gmail.com>
2013-08-20 23:51:07 -0700

    fixed ajax call and adjusted form input length

--- public/js/application.js
+++ public/js/application.js
@@ -6,16 +6,17 @@ $(document).ready(function() {
 		.done(function(data){
 			$('.survey-name').hide();
 				$('.survey-questions').html(data);
+
+				$('button[name="add_q"]').on('click', function(e){
+					e.preventDefault();
+					$('.questions').append('<div class="question"><span>Question : </span><input type="text" name="survey[][title]" style="width: 70%;"><div class="answers"><input type="text" name="survey[][answers][]" placeholder="answer"> <input type="text" name="survey[][answers][]" placeholder="answer"></div><button type="submit" name="add_a">Add Answer</button></div>');
+				});
+
+				$('.questions').on('click', 'button[name="add_a"]', function(e){
+					e.preventDefault();
+					$(this).parent().find('.answers').append(' <input type="text" name="survey[][answers][]" placeholder="answer">');
+				});
+
 		});
 	});
-
-	 $('button[name="add_q"]').on('click', function(e){
-	 	e.preventDefault();
-	 	$('.questions').append('<div class="question"><span>Question : </span><input type="text" name="survey[][title]"><div class="answers"><input type="text" name="survey[][answers][]" placeholder="answer"> <input type="text" name="survey[][answers][]" placeholder="answer"></div><button type="submit" name="add_a">Add Answer</button></div>');
-	 });
-
-	 $('.questions').on('click', 'button[name="add_a"]', function(e){
-	 	e.preventDefault();
-	 	$(this).parent().find('.answers').append(' <input type="text" name="survey[][answers][]" placeholder="answer">');
-	 });
 });
